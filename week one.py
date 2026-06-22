3e11c857c805e04eae4b549896b035b7674d532b
manyoka001 <shikuannen@gmail.com>
2026-06-22 13:57:41 +0300

    add initial implementation of week one script

---
+++ week one.py
@@ -0,0 +1,50 @@
+print("hello word")
+print ("Good morning")
+print("Anne Wanjiku")
+name = "Anne "
+
+greeting = "Jambo"
+add=name + greeting 
+print(f"My name is {add}") 
+
+age = 29
+height =5.3 
+print(f"I am {age} years old and my height is {height} meters.")
+fruits = ["avocads", "passion fruit", "pineapple"]
+student = {
+	"name": "Anne",
+	"age": 29,
+	"course": "Python"
+}
+print (f"favorite fruits: {fruits}")
+print (f"student details: {student}")
+is_student = True #1
+is_teacher = False
+is_graduate = False #0
+print(f"Is a student: {is_student}")
+print(f"Is a graduate: {is_graduate}")
+
+a = 10
+b = 3
+c = a+b
+stringtoprint = f"Multiplication: {a} * {b} = {a * b}"
+print(f"Addition: {a} + {b} = {a + b}")
+print(f"Subtraction: {a} - {b} = {a + b}")
+print(f"Multiplication: {a} * {b} = {a * b}")
+print(stringtoprint)
+print(f"Modulus: {a} % {b} = {a % b}")
+print(f"Exponentiation: {a} ** {b} = {a ** b}")
+x = 5
+y = 10
+print(f"Is x not equal to y? {x != y}")
+print(f"Is x equal to y? {x == y}")
+print(f"Is x less than y? {x < y}")
+print(f"Is x greater than or equal to y? {x >= y}")
+
+string1 = "anne"
+string2 = "wanjiku"
+print(f"is string1 equal to string2? {string1 == string2}")  #!= is the not equals sign, and == checks if 2 variables are equal
+age = input("what is your age? ")
+print (f"you are {age} years old")
+name = input("what is your name? ")
+print(f"your name is {name}")
